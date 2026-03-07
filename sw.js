e5d3717f364f5c0c260fd785c68b60ae9ce87988
morow01 <morow01@gmail.com>
2026-03-07 22:18:25 +0000

    v3.6.7 - fixed application version number string

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'timesheet-v86';
+const CACHE = 'timesheet-v87';
 
 // Detect base path dynamically — works on GitHub Pages AND localhost
 const BASE = self.location.pathname.replace(/sw\.js$/, '');
